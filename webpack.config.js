f4570d27fb5dd3b272e58349c15920f156962a90
mohith2017 <mohith.m.venkat@gmail.com>
2023-06-11 20:43:47 -0400

    Payments website finally working

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  target: 'node',
+  externals: [nodeExternals()],
+  mode: 'production',
+  entry: './server.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'server.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
